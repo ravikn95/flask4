ebc46bc686ca6d4005a2b43e4c451c80d9f3f441
Ravikrishna <70562983+ravikn95@users.noreply.github.com>
2022-05-30 19:03:16 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify, request, make_response
+import os
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET', 'POST'])
+def home():
+	if request.method == 'GET':
+		data = "Hello World!"
+		return jsonify({'data': data})
+	else:
+		Post_Data = "Hello, This is a POST response"
+		return make_response(jsonify({"Data":Post_Data}), 200)
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=80)
